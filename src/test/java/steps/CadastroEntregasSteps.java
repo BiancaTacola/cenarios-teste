71ac4f769445c5e75a3c4ce47bd13c010b0de326
BiancaTacola <biancae.souzaa@gmail.com>
2024-10-22 15:32:00 -0300

    Teste de Pipeline

--- src/test/java/steps/CadastroEntregasSteps.java
+++ src/test/java/steps/CadastroEntregasSteps.java
@@ -1,4 +1,5 @@
 package steps;
+import com.networknt.schema.ValidationMessage;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Então;
@@ -6,8 +7,12 @@ import io.cucumber.java.pt.Quando;
 import model.ErrorMessageModel;
 import org.junit.Assert;
 import services.CadastroEntregasService;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
 public class CadastroEntregasSteps {
     CadastroEntregasService cadastroEntregasService = new CadastroEntregasService();
     @Dado("que eu tenha os seguintes dados da entrega:")
@@ -40,5 +45,16 @@ public class CadastroEntregasSteps {
     @Quando("eu enviar a requisição com o ID para o endpoint {string} de deleção de entrega")
     public void euEnviarARequisiçãoComOIDParaOEndpointDeDeleçãoDeEntrega(String endPoint) {
         cadastroEntregasService.deleteDelivery(endPoint);
+    }
+
+    @E("que o arquivo de contrato esperado é o {string}")
+    public void queOArquivoDeContratoEsperadoÉO(String contract) throws IOException {
+        cadastroEntregasService.setContract(contract);
+    }
+
+    @Então("a resposta da requisição deve estar em conformidade com o contrato selecionado")
+    public void aRespostaDaRequisiçãoDeveEstarEmConformidadeComOContratoSelecionado() throws IOException {
+        Set<ValidationMessage> validateResponse = cadastroEntregasService.validateResponseAgainstSchema();
+        Assert.assertTrue("O contrato está inválido. Erros encontrados: " + validateResponse, validateResponse.isEmpty());
     }
 }
